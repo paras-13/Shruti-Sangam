c6c0a53b4c8f88bf324dd419111d9f243391f521
Paras Upadhyay <paras13upadhyay@gmail.com>
2024-05-01 16:04:00 +0530

    update

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/ShrutiSangam")
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_URI)
 .then(()=> {
     console.log(`Connection Successfull`);
 })
